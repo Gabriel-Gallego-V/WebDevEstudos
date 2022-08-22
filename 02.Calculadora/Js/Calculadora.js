e3d990b4bad477b14ef41e3ff9e4549e3b63fd65
Gabriel Gallego Costa <gabrielgccontato@gmail.com>
2022-08-22 16:06:31 -0300

    Add files via upload

---
+++ 02.Calculadora/Js/Calculadora.js
@@ -0,0 +1,207 @@
+
+let btnOP;
+let btn;
+let op;
+
+document.getElementById("Btn9").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn9").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn8").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn8").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn7").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn7").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn6").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn6").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn5").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn5").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn4").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn4").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn3").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn3").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn2").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn2").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn1").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn1").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn0").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn0").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+
+//Botões para inserir operadores
+
+document.getElementById("BtnPls").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("BtnPls").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Btn-").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Btn-").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Multi").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Multi").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Div").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Div").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("BtnIg").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    let resultado = eval(op);
+    document.getElementById("CalcCont").innerHTML = resultado;
+}
+document.getElementById("BtnRes").onclick = function(){
+    document.getElementById("CalcCont").innerHTML = " ";
+
+}
+document.getElementById("Aspa1").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Aspa1").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Aspa2").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Aspa2").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Expo").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Expo").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Amogus").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Amogus").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
+document.getElementById("Dec").onclick = function(){
+    op = document.getElementById("CalcCont").innerHTML;
+    if(op.length > 14){ 
+        return 0;
+    }else{
+        btn = document.getElementById("Dec").value;
+        document.getElementById("CalcCont").innerHTML = op + btn;
+    }
+
+}
